a27213542444b5dbf70a1d617f14e61caf88129f
Mel28vin <jebasamuel@gmail.com>
2022-06-05 13:42:41 +0530

    UI Changes

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -61,10 +61,10 @@ const Filters = () => {
           <input
             type="checkbox"
             value=""
-            id="toggle"
+            id="toggle-button"
             className="sr-only peer"
           />
-          <div className="w-11 h-6 bg-gray-400 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-black">
+          <div className="w-11 h-6 bg-gray-400 peer-focus:outline-none rounded-full peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-black">
             {" "}
           </div>
           <span className="ml-3">Remote Friendly</span>
